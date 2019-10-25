118fbfb80799fce520c592be9178990e0f7efc59
PauCampos <pau.campos89@gmail.com>
2019-10-25 18:51:27 -0300

    sin interceptores

---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor(private http: HttpClient) { }
+
+  obtenerUsuarios() {
+    let params = new HttpParams().append('page', '1');
+    params = params.append('nombre', 'Paula Campos');
+
+    // Enviar por header
+    const headers = new HttpHeaders({
+      'token-usuario': 'ABASD34634634346GSDFSD45'
+    });
+
+    return this.http.get('https://reqres7uu.in/api/user', {
+      params,
+      headers
+    }).pipe(
+      map((res: any) => res['data']),
+
+      catchError(this.manejarError)
+      );
+  }
+
+  manejarError(error: HttpErrorResponse) {
+    console.log('sucedio un error');
+    console.log('Registro un log file');
+    console.warn(error);
+    return throwError('Error personalizado');
+  }
+}
